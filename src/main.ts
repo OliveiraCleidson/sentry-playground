007e97bac33ae2e559afad0668daca819f8cbdae
Cleidson Oliveira <cleidson.oliveira@wisereducacao.com>
2023-10-30 13:51:10 -0300

    refactor: updated bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+import { applicationFactory } from './factories';
+
+const packageJSON: { version: string } = JSON.parse(
+  readFileSync(resolve(process.cwd(), 'package.json'), 'utf8'),
+);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const [app, logger] = await applicationFactory(packageJSON.version);
+
+  const port = process.env.PORT || 3000;
+  logger.log(
+    `Application is running on: ${port}, environment: ${process.env.NODE_ENV}`,
+  );
+  await app.listen(port);
 }
+
 bootstrap();
